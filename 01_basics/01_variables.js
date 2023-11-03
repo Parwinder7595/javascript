63aa2733914e8373cc1481d32b6a16dd8275769e
Parwinder7595 <143589012+Parwinder7595@users.noreply.github.com>
2023-11-03 11:34:33 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId = 1478
 let accountEmail = "Parwindersingh7595@gmail.com"
 var accountPassword = "123123"
 accountCity = "Delhi"
+accountState();
 
 // accountId = 2//
 accountEmail = "hc.hc@gmail.com"
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+//alert(9+9) // we are using nodejs, not browser
+console.log(9+9);
+console.log("hitesh");
+
+let name ="Parwinder" //string
+let age = 19
+let isLoggedIn = False // boolean
+
+//number= 2 to power53
+//bigint
+//string
+//boolean =true or false
+//null= stand alone value
+//undefined= 
+//symbol= uniqueness
+
+//object
+
+console.log(typeof"hitesh");
