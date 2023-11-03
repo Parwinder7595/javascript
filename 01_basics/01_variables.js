cfd54a0aa8ea47341acd597290bde04abe86e015
Parwinder7595 <143589012+Parwinder7595@users.noreply.github.com>
2023-11-03 10:59:18 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1478
+let accountEmail = "Parwindersingh7595@gmail.com"
+var accountPassword = "123123"
+accountCity = "Delhi"
+
+// accountId = 2//
+accountEmail = "hc.hc@gmail.com"
+accountPassword = "1234567"
+accountCity = "jaiput"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
