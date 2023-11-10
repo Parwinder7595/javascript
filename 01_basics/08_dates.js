aa71d1e7321ab3dbad37c30717e0fcf020d342d9
Parwinder7595 <143589012+Parwinder7595@users.noreply.github.com>
2023-11-10 21:04:50 +0000

    container config done

---
+++ 01_basics/08_dates.js
@@ -0,0 +1,6 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toLocaleDateString());
+
+let myCreatedDate = new Date(2023,10,0)
+console.log(myCreatedDate.toDateString());
