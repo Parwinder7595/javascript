d6dca1e0c48da2a78e9e1f65c99ddedcab54d639
Parwinder7595 <143589012+Parwinder7595@users.noreply.github.com>
2023-11-10 20:52:22 +0000

    container config done

---
+++ 01_basics/07_numsandmaths.js
@@ -0,0 +1,22 @@
+const score =400
+console.log(score);
+
+const balance = new Number(102)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+const otherNumber = 11123.896547
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+console.log(Math);
+console.log((Math.random()*10) + 1 );
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min )
