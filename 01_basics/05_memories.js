0751b33b5a6dc21e0d45d52fb23af0d3b5eb9729
Parwinder7595 <143589012+Parwinder7595@users.noreply.github.com>
2023-11-10 14:49:29 +0000

    container config done

---
+++ 01_basics/05_memories.js
@@ -0,0 +1,24 @@
+// memoeries
+// stack (primitive) heap (non primitive)
+// primitive ( string, number, boolean, null,undefined, BigInt, symbol)
+//non primitive (arrays, objects, functions)
+
+let myyoutubename = "Parwinder"
+
+let anothername = "Parwinder"
+anothername = "gurdeep"
+
+console.log(myyoutubename);
+
+console.log(anothername);
+
+let user1 = {
+    email: "myaqaz",
+    age: "18",
+}
+
+let user2 = user1
+user2.email = "parwinder"
+
+console.log(user1.email);
+console.log(user2.email);
